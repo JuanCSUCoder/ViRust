4d269211055f5962bb5ab7a882391906f641ae5e
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-06-16 12:52:43 -0500

    Listo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::collections::LinkedList;
+use std::{collections::LinkedList, io::{self, Read}};
 
 use clap::Parser;
+use log::info;
 
 /// CLI Arguments
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(author, version, about)]
 struct Arguments {
     /// Gigas of RAM to be filled
@@ -21,11 +22,20 @@ struct Arguments {
 
 fn main() {
     let args = Arguments::parse();
+    pretty_env_logger::init();
+
+    info!("Arguments: {:?}", args);
 
     let mut data: LinkedList<i64> = LinkedList::from([69]);
     let segmentos: u64 = (100 + (args.kilos * 1000) + ((args.megas * 1000000.0) as u64) + ((args.gigas * 1000000000.0) as u64))/8;
 
+    info!("Segments to Fill: {}", segmentos);
+
     for _ in 0..segmentos {
         data.push_back(rand::random());
     }
+
+    info!("Bytes Filled: {}", segmentos*8);
+    info!("Press any key to free the memory");
+    io::stdin().read(&mut [0u8]).unwrap();
 }
